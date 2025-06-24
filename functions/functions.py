0db274aea05f63f9a2fda3b0f824947319cbf006
shawal0909 <shawalolul78@gmail.com>
2025-06-24 12:08:33 +0300

    changed file location

---
+++ functions/functions.py
@@ -0,0 +1,31 @@
+#defining a function that prints out something
+def district(district_name, location = "TORORO"):   
+    print(f"My District is {district_name}, located in {location}")
+
+#function call
+district("SCIT")
+
+
+#parenthesis()
+#parameters: are function varibles 
+#argument: is a value
+
+
+#function that returns something
+def add(a,b):
+    return a + b
+
+result1 = add(5,7)
+print(result1)
+
+#concatenation
+result1 = add("5","7")
+print(result1)
+
+result2 = add("Nabulya ","Ruth")
+print(result2)
+
+result3 = add("Olul ","Shawal")
+print(result3)
+
+
